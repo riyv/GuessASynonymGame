1109a4a6916af905f38f1e7cdc0bc07ebaa7f6c4
riyv <riyag3@illinois.edu>
2019-12-08 22:42:37 -0600

    added playAgain buttons to win and lose

--- app/src/main/java/com/example/guessasynonym/lose.java
+++ app/src/main/java/com/example/guessasynonym/lose.java
@@ -1,13 +1,26 @@
 package com.example.guessasynonym;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class lose extends AppCompatActivity {
+    private Button playAgain;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lose);
+
+        playAgain = findViewById(R.id.playAgain);
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(lose.this, MainActivity.class);
+                startActivity(goBack);
+            }
+        });
 
 
 
--- app/src/main/java/com/example/guessasynonym/win.java
+++ app/src/main/java/com/example/guessasynonym/win.java
@@ -1,13 +1,26 @@
 package com.example.guessasynonym;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class win extends AppCompatActivity {
+    private Button playAgain;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.win);
+
+        playAgain = findViewById(R.id.playAgain);
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(win.this, MainActivity.class);
+                startActivity(goBack);
+            }
+        });
 
 
 
